046b01ecbe419bd6e2fbbf8da4f2acc9455c2012
FBE <frederic.beaupere@baslerhofmann.ch>
2024-03-01 14:47:35 +0100

    fix vendor lib update

--- VendorRevitPythonHelper.lib/vrph/utils.py
+++ VendorRevitPythonHelper.lib/vrph/utils.py
@@ -85,3 +85,81 @@ def get_latest_file_in_dir_by_iso_date_and_extension(search_dir, extension):
     latest_file = found_paths[max(found_paths)]
     # print("found latest file: {}".format(latest_file))
     return latest_file
+
+
+def check_mpxj_lib_available(mpxj_dir):
+    """
+    Check if mpxj lib is installed. If not error out and
+    inform the user to run mpxj boostrap.
+    :param mpxj_dir:
+    :return:
+    """
+    existing_files = []
+    print("INFO: check if mpxj lib download is required..")
+    for node in mpxj_dir.iterdir():
+        if node.is_file():
+            existing_files.append(node.name)
+    required_count = len(REQUIRED_DLLS["mpxj"])
+    found_count = 0
+    for path in REQUIRED_DLLS["mpxj"]:
+        file_name = path.split("/")[-1]
+        if file_name in existing_files:
+            found_count += 1
+    if found_count == required_count:
+        return True
+    else:
+        exit_on_error("mpxj lib is missing - please install it, using 'pyRevit / info / Bootstrap_mpxj'")
+
+
+REQUIRED_DLLS = {
+    "mpxj": [
+        "mpxj/src.net/lib/net45/commons-collections4-4.4.dll",
+        "mpxj/src.net/lib/net45/commons-io-2.11.0.dll",
+        "mpxj/src.net/lib/net45/commons-lang3-3.10.dll",
+        "mpxj/src.net/lib/net45/commons-logging-1.2.dll",
+        "mpxj/src.net/lib/net45/commons-math3-3.6.1.dll",
+        "mpxj/src.net/lib/net45/IKVM.AWT.WinForms.dll",
+        "mpxj/src.net/lib/net45/IKVM.OpenJDK.Beans.dll",
+        "mpxj/src.net/lib/net45/IKVM.OpenJDK.Charsets.dll",
+        "mpxj/src.net/lib/net45/IKVM.OpenJDK.Cldrdata.dll",
+        "mpxj/src.net/lib/net45/IKVM.OpenJDK.Corba.dll",
+        "mpxj/src.net/lib/net45/IKVM.OpenJDK.Core.dll",
+        "mpxj/src.net/lib/net45/IKVM.OpenJDK.Jdbc.dll",
+        "mpxj/src.net/lib/net45/IKVM.OpenJDK.Localedata.dll",
+        "mpxj/src.net/lib/net45/IKVM.OpenJDK.Management.dll",
+        "mpxj/src.net/lib/net45/IKVM.OpenJDK.Media.dll",
+        "mpxj/src.net/lib/net45/IKVM.OpenJDK.Misc.dll",
+        "mpxj/src.net/lib/net45/IKVM.OpenJDK.Naming.dll",
+        "mpxj/src.net/lib/net45/IKVM.OpenJDK.Nashorn.dll",
+        "mpxj/src.net/lib/net45/IKVM.OpenJDK.Remoting.dll",
+        "mpxj/src.net/lib/net45/IKVM.OpenJDK.Security.dll",
+        "mpxj/src.net/lib/net45/IKVM.OpenJDK.SwingAWT.dll",
+        "mpxj/src.net/lib/net45/IKVM.OpenJDK.Text.dll",
+        "mpxj/src.net/lib/net45/IKVM.OpenJDK.Tools.dll",
+        "mpxj/src.net/lib/net45/IKVM.OpenJDK.Util.dll",
+        "mpxj/src.net/lib/net45/IKVM.OpenJDK.XML.API.dll",
+        "mpxj/src.net/lib/net45/IKVM.OpenJDK.XML.Bind.dll",
+        "mpxj/src.net/lib/net45/IKVM.OpenJDK.XML.Crypto.dll",
+        "mpxj/src.net/lib/net45/IKVM.OpenJDK.XML.Parse.dll",
+        "mpxj/src.net/lib/net45/IKVM.OpenJDK.XML.Transform.dll",
+        "mpxj/src.net/lib/net45/IKVM.OpenJDK.XML.WebServices.dll",
+        "mpxj/src.net/lib/net45/IKVM.OpenJDK.XML.XPath.dll",
+        "mpxj/src.net/lib/net45/IKVM.Reflection.dll",
+        "mpxj/src.net/lib/net45/IKVM.Runtime.dll",
+        "mpxj/src.net/lib/net45/IKVM.Runtime.JNI.dll",
+        "mpxj/src.net/lib/net45/ikvm-native-win32-x64.dll",
+        "mpxj/src.net/lib/net45/ikvm-native-win32-x86.dll",
+        "mpxj/src.net/lib/net45/jackcess-4.0.1.dll",
+        "mpxj/src.net/lib/net45/jsoup-1.15.3.dll",
+        "mpxj/src.net/lib/net45/junit.dll",
+        "mpxj/src.net/lib/net45/log4j-api-2.17.2.dll",
+        "mpxj/src.net/lib/net45/mpxj.dll",
+        "mpxj/src.net/lib/net45/mpxj-for-csharp.dll",
+        "mpxj/src.net/lib/net45/mpxj-for-vb.dll",
+        "mpxj/src.net/lib/net45/mpxj-test.dll",
+        "mpxj/src.net/lib/net45/MpxjUtilities.dll",
+        "mpxj/src.net/lib/net45/poi-5.2.2.dll",
+        "mpxj/src.net/lib/net45/rtfparserkit-1.16.0.dll",
+        "mpxj/src.net/lib/net45/sqlite-jdbc-3.42.0.0.dll",
+    ]
+}
