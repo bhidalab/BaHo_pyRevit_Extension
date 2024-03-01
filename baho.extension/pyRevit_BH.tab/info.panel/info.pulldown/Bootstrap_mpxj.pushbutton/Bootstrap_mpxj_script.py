e6d2dea9250455d55374772df3e1f6d69b2a5d58
FBE <frederic.beaupere@baslerhofmann.ch>
2024-03-01 14:20:35 +0100

    add run bootstrap as lib function

--- baho.extension/pyRevit_BH.tab/info.panel/info.pulldown/Bootstrap_mpxj.pushbutton/Bootstrap_mpxj_script.py
+++ baho.extension/pyRevit_BH.tab/info.panel/info.pulldown/Bootstrap_mpxj.pushbutton/Bootstrap_mpxj_script.py
@@ -1,6 +1,11 @@
 """
 Runs the bootstrap process for retrieving mpxj libs needed for reading .mpp files.
 """
-from vrph import bootstrap_mpxj_lib
+from vrph import bootstrap_mpxj_lib, utils
 
-print("testing mpxj bootstrap")
+
+stopwatch = utils.start_script_timer()
+
+bootstrap_mpxj_lib.run_bootstrap()
+
+utils.end_script_timer(stopwatch)
